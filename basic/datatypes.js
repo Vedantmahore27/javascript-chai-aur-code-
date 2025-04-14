18c67c94d66c47741d0d4f6937180135dd61af9d
Vedantmahore27 <145027745+Vedantmahore27@users.noreply.github.com>
2025-04-14 12:15:29 +0000

    datatype and variable

---
+++ basic/datatypes.js
@@ -0,0 +1,21 @@
+'use strict'; //treat all JS code as newer version
+
+//alert(3+3) we are not  using browser
+console.log(3 + 3)
+let name="vedant"
+let age= 45
+let isLoggedIn=true
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean =>true,false
+//null =>standard value
+//undefined => 
+//symbol =>unique
+
+//object
+
---
+++ basic/variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "vedantmahore27@gmail.com"
+var accountPassword ="12345"
+accountCity ="amravati"  
+let accountState;
+
+// accountId=2   const updation not allowed
+//var-scope proble
+//let use 
+/*prefer not to use war because issue in block scope and functional scope                         */
+
+
+
+
+accountEmail ="ved@45"
+accountPassword="4567"
+accountCity="bengluru"
+console.log(accountId)
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
